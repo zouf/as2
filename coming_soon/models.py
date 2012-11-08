876ec5ea9fd55c38aa30ac858ce0d26f1249d023
zouf <mattzouf@gmail.com>
2012-11-08 13:52:09 -0500

    forms

--- coming_soon/models.py
+++ coming_soon/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.localflavor.us.us_states import STATE_CHOICES
+from django.contrib.localflavor.us.models import USStateField
+from django.forms import ModelForm
 # Create your models here.
+
+
+class InterestedUser(models.Model):
+    first_name = models.CharField(_('first name'), max_length=30, blank=True, null=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True, null=True)
+    email = models.EmailField(_('e-mail address'))
+    city = models.CharField(_('city'), max_length=50, blank=True, null=True)
+    state = USStateField(choices = STATE_CHOICES, blank=True,null=True)
+
+    def __unicode__(self):
+        return self.email
+
+
+class InterestedUserForm(ModelForm):
+    class Meta:
+        model = InterestedUser
+        exclude = ('first_name','last_name')
--- coming_soon/views.py
+++ coming_soon/views.py
@@ -1,24 +1,35 @@
 # Create your views here.
+from coming_soon.models import InterestedUserForm, InterestedUser
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-try:
-    import json
-except ImportError:
-    import simplejson as json
-from api.views import *
+
+
+
 def coming_soon(request):
-    return render_to_response('coming_soon.html', context_instance=RequestContext(request))    
+    interesteduser = InterestedUser()
+    if request.method == "POST":
+        formset = InterestedUserForm(request.POST)
+        if formset.is_valid():
+            formset.save()
+            # Do something. Should generally end with a redirect. For example:
+            return HttpResponseRedirect('allsortz.com')
+    else:
+        formset = InterestedUserForm(instance=interesteduser)
+    return render_to_response('coming_soon.html', {
+        "formset": formset,
+    })    
 
 def index(request):
     return render_to_response('index.html', context_instance=RequestContext(request))    
  
-#experimenting with web version of app
-def detail(request,oid):
-    req =  get_business(request,oid);
-    context = json.loads(req.content)
-    return render_to_response('detail.html', context, context_instance=RequestContext(request)) 
+##experimenting with web version of app
+#def detail(request,oid):
+#    req =  get_business(request,oid);
+#    context = json.loads(req.content)
+#    return render_to_response('detail.html', context, context_instance=RequestContext(request)) 
 
 def health_survey(request):
-  return render_to_response('health.html', context_instance=RequestContext(request))    
+    return render_to_response('health.html', context_instance=RequestContext(request))    
 
 
